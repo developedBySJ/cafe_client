95db31f48233efd4e48c0b90baa7ed85719b58dc
developedbysj <developedbysj@gmail.com>
2021-07-06 18:37:46 +0530

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Button from "@material-ui/core/Button";
+import Button from '@material-ui/core/Button'
 export const App = () => {
   return (
     <Button variant="contained" color="primary">
       Hello World
     </Button>
-  );
-};
+  )
+}
 
-export default App;
+export default App
