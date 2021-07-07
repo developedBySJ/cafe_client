1f93e267fb4b46bfa369e4e3582c18ae2f9be67c
developedbysj <developedbysj@gmail.com>
2021-07-07 20:10:54 +0530

    Add QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,60 @@
 import { ThemeProvider, CssBaseline, Container, makeStyles } from '@material-ui/core'
 import { Login, SignUp } from './section'
+import { Navbar } from './lib/component'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { PegasusUI } from './Theme'
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
 
 const useStyle = makeStyles((theme) => ({
   container: {
+    minHeight: '200vh',
+  },
+  wrapper: {
     backgroundColor: theme.palette.grey[100],
-    minHeight: '100vh',
+    paddingTop: '56px',
+    [theme.breakpoints.up('sm')]: {
+      paddingTop: '64px',
+    },
   },
 }))
 
 export const App = () => {
-  const { container } = useStyle()
+  const { container, wrapper } = useStyle()
   return (
-    <Switch>
-      <Container maxWidth="xl" className={container}>
-        <Route path="/login" exact>
-          <Login />
-        </Route>
-        <Route path="/signup" exact>
-          <SignUp />
-        </Route>
-      </Container>
-    </Switch>
+    <div id="my-app-wrapper" className={wrapper}>
+      <Navbar />
+      <Switch>
+        <Container maxWidth="xl" className={container}>
+          <Route path="/login" exact>
+            <Login />
+          </Route>
+          <Route path="/signup" exact>
+            <SignUp />
+          </Route>
+        </Container>
+      </Switch>
+    </div>
   )
 }
 
+const queryClient = new QueryClient()
+
 const AppWrapper = () => {
   return (
-    <ThemeProvider theme={PegasusUI}>
-      <CssBaseline />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={PegasusUI}>
+        <CssBaseline />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
 
