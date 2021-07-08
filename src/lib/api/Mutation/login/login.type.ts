58ed5e5018bd136460e84a68689a485488c6a266
developedbysj <developedbysj@gmail.com>
2021-07-08 18:08:41 +0530

    Update types

--- src/lib/api/Mutation/login/login.type.ts
+++ src/lib/api/Mutation/login/login.type.ts
@@ -1,19 +1,9 @@
 import { UserRole } from '../../../types'
+import { IUser } from '../../types'
 
 export interface LoginPayload {
   email: string
   password: string
 }
 
-export interface LoginResponse {
-  id: string
-  firstName: string
-  lastName: string
-  email: string
-  dateOfBirth: Date | null
-  role: UserRole
-  address: string
-  avatar: string
-  createdAt: Date
-  updatedAt: Date
-}
+export type LoginResponse = IUser
