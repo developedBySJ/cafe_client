f0b79bcf14622e67eb73a012d6dc58091546477b
developedbysj <developedbysj@gmail.com>
2021-07-13 14:46:54 +0530

    Update MENU_ITEMS query function to async function

--- src/lib/api/query/menuItems/menuItems.ts
+++ src/lib/api/query/menuItems/menuItems.ts
@@ -3,9 +3,9 @@ import { MenuItemsQuery, MenuItemsResponse } from './menuItems.type'
 
 export const MENU_ITEMS_URL = '/api/v1/menu-items'
 
-export const MENU_ITEMS = (
+export const MENU_ITEMS = async (
   params: MenuItemsQuery | undefined,
 ): Promise<AxiosResponse<MenuItemsResponse>> =>
-  axios.get(MENU_ITEMS_URL, {
+  await axios.get(MENU_ITEMS_URL, {
     params,
   })
