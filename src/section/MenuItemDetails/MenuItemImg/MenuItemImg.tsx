9a98b1000a3c01aee26d5e8c3244ff7d5b320cdf
developedbysj <developedbysj@gmail.com>
2021-07-22 20:56:09 +0530

    Add responsive product images

--- src/section/MenuItemDetails/MenuItemImg/MenuItemImg.tsx
+++ src/section/MenuItemDetails/MenuItemImg/MenuItemImg.tsx
@@ -1,12 +1,15 @@
-import { Box, Grid } from '@material-ui/core'
+import { Box, Grid, useMediaQuery, useTheme } from '@material-ui/core'
 import React from 'react'
 import { Slider } from '../../../lib/components'
 import { AspectRatioBox } from '../../../lib/components/AspectRatioBox'
 
 export const MenuItemImg: React.FC<{ images: string[] }> = ({ images }) => {
+  const theme = useTheme()
+  const matches = useMediaQuery(theme.breakpoints.down('sm'))
   return (
     <div>
-      {/* <Slider
+      <Box hidden={!matches}>
+        <Slider
           components={images.map((i) => (
             <AspectRatioBox ratio={845 / 564}>
               <img
@@ -21,30 +24,15 @@ export const MenuItemImg: React.FC<{ images: string[] }> = ({ images }) => {
               />
             </AspectRatioBox>
           ))}
-        /> */}
-      <Grid container spacing={1}>
-        {images.length === 1 && (
-          <Grid item xs={12}>
-            <AspectRatioBox ratio={845 / 564}>
-              <img
-                src={images[0]}
-                alt=""
-                style={{
-                  width: '100%',
-                  height: '100%',
-                  objectFit: 'contain',
-                  objectPosition: 'center',
-                }}
-              />
-            </AspectRatioBox>
-          </Grid>
-        )}
-        {images.length === 2 &&
-          images.map((i, index) => (
+        />
+      </Box>
+      <Box hidden={matches}>
+        <Grid container spacing={1}>
+          {images.length === 1 && (
             <Grid item xs={12}>
               <AspectRatioBox ratio={845 / 564}>
                 <img
-                  src={i}
+                  src={images[0]}
                   alt=""
                   style={{
                     width: '100%',
@@ -55,10 +43,61 @@ export const MenuItemImg: React.FC<{ images: string[] }> = ({ images }) => {
                 />
               </AspectRatioBox>
             </Grid>
-          ))}
-        {images.length === 3 && (
-          <>
-            {images.slice(0, -1).map((i, index) => (
+          )}
+          {images.length === 2 &&
+            images.map((i, index) => (
+              <Grid item xs={12}>
+                <AspectRatioBox ratio={845 / 564}>
+                  <img
+                    src={i}
+                    alt=""
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      objectFit: 'contain',
+                      objectPosition: 'center',
+                    }}
+                  />
+                </AspectRatioBox>
+              </Grid>
+            ))}
+          {images.length === 3 && (
+            <>
+              {images.slice(0, -1).map((i, index) => (
+                <Grid item key={index} xs={6}>
+                  <AspectRatioBox ratio={845 / 564}>
+                    <img
+                      src={i}
+                      alt=""
+                      style={{
+                        width: '100%',
+                        height: '100%',
+                        objectFit: 'contain',
+                        objectPosition: 'center',
+                      }}
+                    />
+                  </AspectRatioBox>
+                </Grid>
+              ))}
+              <Grid item xs={12}>
+                <AspectRatioBox ratio={845 / 564}>
+                  <img
+                    src={images[2]}
+                    alt=""
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      objectFit: 'contain',
+                      objectPosition: 'center',
+                    }}
+                  />
+                </AspectRatioBox>
+              </Grid>
+            </>
+          )}
+
+          {images.length > 3 &&
+            images.map((i, index) => (
               <Grid item key={index} xs={6}>
                 <AspectRatioBox ratio={845 / 564}>
                   <img
@@ -74,41 +113,8 @@ export const MenuItemImg: React.FC<{ images: string[] }> = ({ images }) => {
                 </AspectRatioBox>
               </Grid>
             ))}
-            <Grid item xs={12}>
-              <AspectRatioBox ratio={845 / 564}>
-                <img
-                  src={images[2]}
-                  alt=""
-                  style={{
-                    width: '100%',
-                    height: '100%',
-                    objectFit: 'contain',
-                    objectPosition: 'center',
-                  }}
-                />
-              </AspectRatioBox>
-            </Grid>
-          </>
-        )}
-
-        {images.length > 3 &&
-          images.map((i, index) => (
-            <Grid item key={index} xs={6}>
-              <AspectRatioBox ratio={845 / 564}>
-                <img
-                  src={i}
-                  alt=""
-                  style={{
-                    width: '100%',
-                    height: '100%',
-                    objectFit: 'contain',
-                    objectPosition: 'center',
-                  }}
-                />
-              </AspectRatioBox>
-            </Grid>
-          ))}
-      </Grid>
+        </Grid>
+      </Box>
     </div>
   )
 }
