bebb8d09ac484c851e9f3ba4c8af241cbd8798d0
developedbysj <developedbysj@gmail.com>
2021-07-22 20:54:37 +0530

    Add Snipper Component

---
+++ src/lib/components/Spinner/Spinner.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Box, CircularProgress, Typography } from '@material-ui/core'
+import { CircularProgressProps } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core'
+
+interface SpinnerProps extends CircularProgressProps {
+  fullWidth?: boolean
+  duration?: number
+  label?: string
+}
+
+const useStyle = makeStyles((theme) => ({
+  fullWidth: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  normal: {
+    textAlign: 'center',
+  },
+  label: {
+    marginTop: '1rem',
+  },
+}))
+
+export const Spinner: React.FC<SpinnerProps> = ({
+  duration = 650,
+  fullWidth = false,
+  label,
+  ...props
+}) => {
+  const classes = useStyle()
+  return (
+    <Box className={fullWidth ? classes.fullWidth : classes.normal}>
+      <CircularProgress style={{ animationDuration: `${duration}ms` }} disableShrink {...props} />
+      {label && (
+        <Typography color="textSecondary" className={classes.label} variant="h6">
+          {label}
+        </Typography>
+      )}
+    </Box>
+  )
+}
