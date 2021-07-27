20a577247b781240ae8aec229b1087e0d56c6e60
developedBySJ <developedbysj@gmail.com>
2021-07-27 16:51:27 +0530

    Add RouteNotImplemented Component

---
+++ src/lib/components/RouteNotImplemented/RouteNotImplemented.tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@material-ui/core'
+import React from 'react'
+
+export const RouteNotImplemented = () => {
+  return (
+    <div>
+      <Typography variant="h4">Route Not Implemented</Typography>
+    </div>
+  )
+}
