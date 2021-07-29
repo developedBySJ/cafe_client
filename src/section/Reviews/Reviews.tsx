7931ab57abe9023c865b53ea14beeb19b4149a2e
developedBySJ <developedbysj@gmail.com>
2021-07-29 16:53:09 +0530

    Add write review button

--- src/section/Reviews/Reviews.tsx
+++ src/section/Reviews/Reviews.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Button,
   Container,
   FormControl,
   Grid,
@@ -24,7 +25,7 @@ export const Reviews = () => {
           <Typography variant="h5">5 Reviews</Typography>
         </Box>
         <Box margin="2rem 0">
-          <Grid container>
+          <Grid container spacing={1} alignItems="center" justifyContent="space-between">
             <Grid item xs={12} md={6} lg={4}>
               <FormControl variant="filled" fullWidth>
                 <InputLabel id="sortBy">Sort By</InputLabel>
@@ -34,6 +35,11 @@ export const Reviews = () => {
                   <MenuItem value={20}>Ratings : Low to High</MenuItem>
                 </Select>
               </FormControl>
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <Button size="large" fullWidth variant="outlined" color="primary">
+                Write Review
+              </Button>
             </Grid>
           </Grid>
         </Box>
