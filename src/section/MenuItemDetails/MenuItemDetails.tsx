f2f9536d8d6488b034fd1fb7f0f9369eb4d432b9
developedbysj <developedbysj@gmail.com>
2021-08-02 23:01:54 +0530

    Use GET_MENU_ITEM query

--- src/section/MenuItemDetails/MenuItemDetails.tsx
+++ src/section/MenuItemDetails/MenuItemDetails.tsx
@@ -11,36 +11,9 @@ import { WARNING_MAIN } from '../../Theme/token'
 import { MenuItemCard, MenuItemSkeleton } from '../MenuItemsList'
 import { MenuItemImg } from './MenuItemImg'
 import { useQuery } from 'react-query'
-
-const mock = {
-  id: '9e4a1aae-20fd-46a7-a80b-eb57649c78b0',
-  title: 'Dulce de Leche Cheesecake Brownie',
-  subTitle: 'World Cuisine',
-  images: [
-    'https://d3gy1em549lxx2.cloudfront.net/12c23c5a-fae4-455e-8f04-79205ef64d00.JPG',
-    'https://d3gy1em549lxx2.cloudfront.net/12c23c5a-fae4-455e-8f04-79205ef64d00.JPG',
-    'https://d3gy1em549lxx2.cloudfront.net/12c23c5a-fae4-455e-8f04-79205ef64d00.JPG',
-  ],
-  isAvailable: true,
-  isVeg: false,
-  price: 149,
-  discount: 10,
-  description:
-    'A decadent moist chocolate fudge brownie with a layer of cheesecake and LOTS of dulce de leche (thickened milk and sugar) to indulge. Sweet caramelly milky and creamy. Heaven!',
-  prepTime: 9,
-  ingredients: [],
-  createdAt: '2021-07-11T17:27:32.931Z',
-  updatedAt: '2021-07-11T17:27:32.931Z',
-  menu: {
-    id: '8cd72eae-53c6-47b8-b749-9ec0f6e2d7ef',
-    name: 'FRESH DESSERTS',
-    isActive: true,
-    image:
-      'https://images.unsplash.com/photo-1582716401301-b2407dc7563d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1054&q=80',
-    createdAt: '2021-07-11T14:42:28.521Z',
-    updatedAt: '2021-07-11T14:42:28.521Z',
-  },
-}
+import { useParams } from 'react-router-dom'
+import { GET_MENU_ITEM } from '../../lib/api/query/menuItemDetail'
+import { useEffect, useState } from 'react'
 
 const useStyle = makeStyles((theme) => ({
   container: {
@@ -78,15 +51,33 @@ const useStyle = makeStyles((theme) => ({
 }))
 
 export const MenuItemDetails = () => {
-  const { images, title, subTitle, description, price, isVeg, prepTime, discount } = mock
-  const classes = useStyle({ isVeg })
-  const { data, isError, isLoading } = useQuery(
-    ['users', {} as MenuItemsQuery],
-    () => MENU_ITEMS({}),
-    {
-      onSuccess: ({ data }) => console.log(data),
-    },
-  )
+  const { id } = useParams<{ id: string }>()
+  const [isVegStyle, setIsVegStyle] = useState(false)
+  const classes = useStyle({ isVeg: isVegStyle })
+  console.log({ id })
+
+  const { data, isError, isLoading, refetch } = useQuery(['id'], () => GET_MENU_ITEM(id), {
+    onSuccess: ({ data }) => setIsVegStyle(data.isVeg),
+  })
+
+  const {
+    data: suggestionData,
+    isError: isSuggestionError,
+    isLoading: isSuggestionLoading,
+  } = useQuery(['users', {} as MenuItemsQuery], () => MENU_ITEMS({}), {
+    onSuccess: ({ data }) => console.log(data),
+  })
+
+  useEffect(() => {
+    refetch()
+  }, [id])
+
+  if (!data) {
+    return <h1>Error</h1>
+  }
+
+  const { images, title, subTitle, description, price, prepTime, discount } = data?.data
+
   return (
     <Container className={classes.container}>
       <Grid container spacing={6}>
@@ -108,8 +99,8 @@ export const MenuItemDetails = () => {
               className={classes.ratingTag}
             />
             <Chip
-              icon={<VegNonVegIcon style={{ marginLeft: '12px' }} isVeg={isVeg} size={16} />}
-              label={isVeg ? 'Pure Veg' : 'Non-Veg'}
+              icon={<VegNonVegIcon style={{ marginLeft: '12px' }} isVeg={isVegStyle} size={16} />}
+              label={isVegStyle ? 'Pure Veg' : 'Non-Veg'}
               variant="default"
               className={classes.typeTag}
             />
@@ -176,9 +167,12 @@ export const MenuItemDetails = () => {
       </Grid>
       <Box marginBottom={'2rem'}>
         <ProductCardSlider
-          cards={data?.data.result.map((menuItem) => <MenuItemCard menuItem={menuItem} />) || []}
-          error={isError}
-          isLoading={isLoading}
+          cards={
+            suggestionData?.data.result.map((menuItem) => <MenuItemCard menuItem={menuItem} />) ||
+            []
+          }
+          error={isSuggestionError}
+          isLoading={isSuggestionLoading}
           skeltonCard={<MenuItemSkeleton />}
           title="People Also Buy"
         />
