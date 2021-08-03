8b872d43dff0f9ec4669a394a27b09527598a9fa
developedbysj <developedbysj@gmail.com>
2021-08-03 11:48:11 +0530

    Add ReviewCardProps to ReviewCard

--- src/lib/components/ReviewCard/ReviewCard.tsx
+++ src/lib/components/ReviewCard/ReviewCard.tsx
@@ -1,28 +1,29 @@
 import React from 'react'
 import { Box, Avatar, Typography, Grid } from '@material-ui/core'
+import { IReview } from '../../api/types/review.type'
 
-export const ReviewCard = () => {
+interface ReviewCardProps {
+  data: IReview
+}
+
+export const ReviewCard: React.FC<ReviewCardProps> = ({ data }) => {
+  const { createdBy, comment, createdAt, ratings, id, title, image } = data
+  const { firstName, lastName, avatar, email } = createdBy
   return (
     <Box marginTop="1rem">
       <Box display="flex">
         <Box marginRight="1rem">
-          <Avatar
-            sizes="48px"
-            style={{ marginTop: 4 }}
-            src={'https://d3gy1em549lxx2.cloudfront.net/12c23c5a-fae4-455e-8f04-79205ef64d00.JPG'}
-          />
+          <Avatar sizes="48px" style={{ marginTop: 4 }} src={avatar} />
         </Box>
         <div>
           <Typography variant="body1" style={{ fontWeight: 500 }}>
-            James Bond
+            {`${firstName || ''} ${lastName || ''}`}
           </Typography>
           <Typography variant="body1" gutterBottom>
-            {new Date().toDateString()}
+            {new Date(createdAt).toDateString()}
           </Typography>
           <Typography variant="body2" style={{ textAlign: 'justify' }}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis doloribus, aspernatur
-            tempora at neque autem temporibus blanditiis, vitae rerum eaque quae. Maxime ullam
-            dolore porro deserunt ad. Laborum, perferendis cumque.
+            {comment}
           </Typography>
         </div>
       </Box>
