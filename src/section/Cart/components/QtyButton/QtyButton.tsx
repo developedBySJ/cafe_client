191efe30075d3be2d8fc275c3b842af1d052ae99
developedbysj <developedbysj@gmail.com>
2021-08-05 20:04:35 +0530

    Disable button for min max qty

--- src/section/Cart/components/QtyButton/QtyButton.tsx
+++ src/section/Cart/components/QtyButton/QtyButton.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Box, Button, IconButton, Typography } from '@material-ui/core'
+import { Box, IconButton, Typography } from '@material-ui/core'
 import { Minus, Plus } from 'react-feather'
 import { makeStyles } from '@material-ui/core'
-import { lighten } from '@material-ui/core'
+import { useMutation, useQueryClient } from 'react-query'
+import { UpdateCartPayload, UPDATE_CART_ITEM } from '../../../../lib/api/Mutation/updateCart'
 
 const useStyle = makeStyles((theme) => ({
   qty: {
@@ -19,17 +20,40 @@ const useStyle = makeStyles((theme) => ({
   minusButton: {},
 }))
 
-export const QtyButton = () => {
+interface QtyButtonProps {
+  qty: number
+  id: string
+}
+
+export const QtyButton: React.FC<QtyButtonProps> = ({ qty, id }) => {
   const classes = useStyle()
+  const queryClient = useQueryClient()
+  const { mutate } = useMutation((data: UpdateCartPayload) => UPDATE_CART_ITEM(data), {
+    onSuccess: () => {
+      queryClient.invalidateQueries('getCart')
+    },
+  })
   return (
     <Box display="flex" alignItems="center" className={classes.wrapper}>
-      <IconButton size="small" color="primary" className={classes.minusButton}>
+      <IconButton
+        size="small"
+        color="primary"
+        className={classes.minusButton}
+        onClick={() => mutate({ id, qty: qty - 1 })}
+        disabled={qty <= 1}
+      >
         <Minus />
       </IconButton>
       <Typography variant="h6" className={classes.qty}>
-        {2}
+        {qty}
       </Typography>
-      <IconButton size="small" color="primary" className={classes.plusButton}>
+      <IconButton
+        size="small"
+        color="primary"
+        className={classes.plusButton}
+        onClick={() => mutate({ id, qty: qty + 1 })}
+        disabled={qty >= 10}
+      >
         <Plus />
       </IconButton>
     </Box>
