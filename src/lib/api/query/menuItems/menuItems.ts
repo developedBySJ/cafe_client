f858003c025f4c88e40e6ea619435039833dcd27
developedBySJ <developedbysj@gmail.com>
2021-08-25 08:51:41 +0530

    Fix bugs

--- src/lib/api/query/menuItems/menuItems.ts
+++ src/lib/api/query/menuItems/menuItems.ts
@@ -6,6 +6,4 @@ export const MENU_ITEMS_URL = '/api/v1/menu-items'
 export const MENU_ITEMS = async (
   params: string | undefined | {},
 ): Promise<AxiosResponse<MenuItemsResponse>> =>
-  await axios.get(MENU_ITEMS_URL, {
-    params,
-  })
+  await axios.get((`${MENU_ITEMS_URL}${params || ""}`))
