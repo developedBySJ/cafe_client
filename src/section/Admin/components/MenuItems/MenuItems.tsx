7b768de74dedd8a3f557af95f45a9f56abff318d
developedBySJ <developedbysj@gmail.com>
2021-08-30 10:34:00 +0530

    Add MenuItem Resource

--- src/section/Admin/components/MenuItems/MenuItems.tsx
+++ src/section/Admin/components/MenuItems/MenuItems.tsx
@@ -1,5 +1,30 @@
-import React from 'react'
+import { GridSortModel } from '@mui/x-data-grid'
+import React, { useState } from 'react'
+import { useQuery } from 'react-query'
+import { GET_MENU_ITEMS } from '../../../../lib/api/query/menuItems'
+import { ResourceTable } from '../../../../lib/components/ResourceTable'
+import { useHandleQueryChange } from '../../../../lib/hooks'
+import { columns } from './config'
 
 export const MenuItems = () => {
-  return <div>menuitems</div>
+  const [query, setQuery] = useState('?limit=25')
+  const { data, isLoading } = useQuery(
+    ['getAllMenuItems', query],
+    (q) => GET_MENU_ITEMS(q.queryKey[1]),
+    {},
+  )
+
+  const { handlePageChange, handleSortChange } = useHandleQueryChange(query, setQuery)
+
+  return (
+    <ResourceTable
+      columns={columns}
+      onPageChange={handlePageChange}
+      onSortChange={handleSortChange}
+      totalCount={data?.data.totalCount}
+      data={data?.data.result || []}
+      isLoading={isLoading}
+      label="Dishes"
+    />
+  )
 }
---
+++ src/section/Admin/components/MenuItems/config.ts
@@ -0,0 +1,77 @@
+import { ResourceTableColumn } from "../../../../lib/components/ResourceTable/type";
+
+export const columns: ResourceTableColumn[] = [
+  {
+    type: 'link',
+    field: 'id',
+    header: 'ID',
+    prefix: '/dishes',
+  },
+  {
+    type: 'string',
+    field: 'title',
+    header: 'Title',
+    width: 200,
+  },
+  {
+    type: 'string',
+    field: 'subTitle',
+    header: 'Sub Title',
+    width: 150,
+  },
+  {
+    type: 'link',
+    field: 'menu.name',
+    header: 'Menu',
+    linkField: 'menu.id',
+    width: 200,
+    prefix: '/menus',
+    filterable: true,
+  },
+  {
+    type: 'boolean',
+    field: 'isAvailable',
+    header: 'Available',
+    width: 150,
+    sortable: true,
+  },
+  {
+    type: 'boolean',
+    field: 'isVeg',
+    header: 'Type',
+    width: 150,
+    trueLabel: 'Veg',
+    falseLabel: 'Non-Veg',
+  },
+  {
+    type: 'money',
+    field: 'price',
+    header: 'Price',
+    precision: 2,
+    prefix: 'Rs. ',
+    width: 150,
+    sortable: true,
+  },
+  {
+    type: 'number',
+    field: 'discount',
+    header: 'Discount',
+    width: 160,
+    precision: 2,
+    sortable: true,
+  },
+  {
+    type: 'number',
+    field: 'prepTime',
+    header: 'Preparation Time',
+    width: 200,
+    sortable: true,
+  },
+  {
+    type: 'date',
+    field: 'createdAt',
+    header: 'Created At',
+    width: 200,
+    sortable: true,
+  },
+]
