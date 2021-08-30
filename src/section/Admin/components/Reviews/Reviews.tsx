242f2e22333e71d62019368176095523d4b99cc9
developedBySJ <developedbysj@gmail.com>
2021-08-30 10:36:21 +0530

    Add Review Resource

--- src/section/Admin/components/Reviews/Reviews.tsx
+++ src/section/Admin/components/Reviews/Reviews.tsx
@@ -1,5 +1,25 @@
-import React from 'react'
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+import { GET_REVIEWS } from '../../../../lib/api/query/reviews'
+import { ResourceTable } from '../../../../lib/components/ResourceTable'
+import { useHandleQueryChange } from '../../../../lib/hooks'
+import { columns } from './config'
 
 export const Reviews = () => {
-  return <div>reviews</div>
+  const [query, setQuery] = useState('?limit=25')
+  const { data, isLoading } = useQuery(['getAllReviews', query], (q) => GET_REVIEWS(q.queryKey[1]))
+
+  const { handlePageChange, handleSortChange } = useHandleQueryChange(query, setQuery)
+
+  return (
+    <ResourceTable
+      columns={columns}
+      data={data?.data.result || []}
+      onPageChange={handlePageChange}
+      onSortChange={handleSortChange}
+      totalCount={data?.data.totalCount}
+      isLoading={isLoading}
+      label="Reviews"
+    />
+  )
 }
---
+++ src/section/Admin/components/Reviews/config.ts
@@ -0,0 +1,47 @@
+import { ResourceTableColumn } from "../../../../lib/components/ResourceTable/type";
+
+export const columns: ResourceTableColumn[] = [
+  {
+    type: 'link',
+    field: "id",
+    header: "ID",
+    prefix: "/reviews"
+  },
+
+  {
+    type: 'link',
+    field: "menuItem.title",
+    header: "Dish",
+    linkField: "menuItem.id",
+    prefix: '/dishes'
+  },
+  {
+    type: 'string',
+    field: "title",
+    header: "Title",
+  },
+  {
+    type: 'string',
+    field: "comment",
+    header: "Comment",
+  },
+  {
+    type: 'number',
+    field: "ratings",
+    header: "Ratings",
+    sortable: true
+  },
+  {
+    type: 'link',
+    field: "createdBy.firstName",
+    linkField: "createdBy.id",
+    header: "Created By",
+    prefix: '/users'
+  },
+  {
+    type: 'date',
+    field: "createdAt",
+    header: "Created At",
+    sortable: true
+  },
+]
