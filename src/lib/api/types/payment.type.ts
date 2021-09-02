34969e9cfbb9f61fc270476dfb8eee95af257edd
developedBySJ <developedbysj@gmail.com>
2021-09-02 18:34:42 +0530

    Add payment types

---
+++ src/lib/api/types/payment.type.ts
@@ -0,0 +1,19 @@
+import { IUser } from "."
+import { IOrder } from "./order.type"
+
+export enum PaymentType {
+  Card = 'card',
+  Cash = 'cash',
+}
+
+export interface IPayment {
+  id: string
+  amount: number
+  referenceId: string
+  order: IOrder
+  type: PaymentType
+  description: string
+  createdBy: IUser
+  createdAt: Date
+  updatedAt: Date
+}
