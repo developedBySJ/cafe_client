f2de1671c05b3a8e396b879ca491b9829f92c2b3
developedBySJ <developedbysj@gmail.com>
2021-09-06 13:56:25 +0530

    Add update order status option

--- src/section/Invoice/Invoice.tsx
+++ src/section/Invoice/Invoice.tsx
@@ -1,11 +1,25 @@
-import { IconButton, makeStyles } from '@material-ui/core'
+import {
+  Button,
+  Chip,
+  FormControl,
+  IconButton,
+  InputLabel,
+  makeStyles,
+  MenuItem,
+  Select,
+} from '@material-ui/core'
 import { Box, Container, Grid, Typography } from '@material-ui/core'
+import { useState } from 'react'
 import { Printer } from 'react-feather'
-import { useQuery } from 'react-query'
-import { useParams, useLocation } from 'react-router-dom'
+import { useMutation, useQuery } from 'react-query'
+import { useParams } from 'react-router-dom'
 import { PrivateRouteComponent } from '../../lib'
+import { UPDATE_ORDER } from '../../lib/api/Mutation/updateOrder'
 import { GET_ORDER_DETAIL } from '../../lib/api/query/orderDetail'
+import { OrderStatus } from '../../lib/api/types/order.type'
 import { GenericTable } from '../../lib/components/GenericTable'
+import { useOnErrorNotify, useOnSuccessNotify } from '../../lib/hooks'
+import { UserRole } from '../../lib/types'
 
 const useStyle = makeStyles((theme) => ({
   heading: {
@@ -27,11 +41,15 @@ const useStyle = makeStyles((theme) => ({
 
 export const Invoice: PrivateRouteComponent = ({ viewer }) => {
   const { id: orderId } = useParams<{ id: string }>()
-  const location = useLocation()
-  const showThankYou = new URLSearchParams(location.search).get('thankYou') === 'true'
   const classes = useStyle()
+  const notifySuccess = useOnSuccessNotify()
+  const notifyError = useOnErrorNotify()
 
   const { data } = useQuery(['getOrderDetails', orderId], () => GET_ORDER_DETAIL(orderId))
+  const { mutate } = useMutation(UPDATE_ORDER, {
+    onSuccess: () => notifySuccess('Order status updated successfully'),
+    onError: notifyError,
+  })
 
   const tableData = data?.data.orderItems.map((item, i) => [
     item.menuItem.title,
@@ -39,6 +57,11 @@ export const Invoice: PrivateRouteComponent = ({ viewer }) => {
     item.menuItem.price,
     item.qty * item.menuItem.price,
   ])
+
+  const orderDetails = data?.data
+
+  const [status, setStatus] = useState(orderDetails?.status || 0)
+
   return (
     <>
       <Box className={classes.heading}>
@@ -50,6 +73,58 @@ export const Invoice: PrivateRouteComponent = ({ viewer }) => {
         </Typography>
       </Box>
       <Container maxWidth="md">
+        {/* ACTIONS */}
+        <Box sx={{ display: 'flex', alignItems: 'center', mt: '2rem' }}>
+          {status < OrderStatus.Delivered ? (
+            <>
+              {([UserRole.Admin, UserRole.Manager] as any[]).includes(viewer?.role) && (
+                <>
+                  <Box sx={{ minWidth: 200, mr: '0.5rem', width: '100%' }}>
+                    <FormControl
+                      fullWidth
+                      variant="filled"
+                      disabled={status >= OrderStatus.Delivered}
+                    >
+                      <InputLabel>Order Status</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        placeholder="Order Status"
+                        fullWidth
+                        variant="filled"
+                        value={status}
+                        onChange={(e) => setStatus(Number(e.target?.value) || 0)}
+                      >
+                        <MenuItem value={OrderStatus.Placed}>Placed</MenuItem>
+                        <MenuItem value={OrderStatus.Confirmed}>Confirmed</MenuItem>
+                        <MenuItem value={OrderStatus.Processed}>Processed</MenuItem>
+                        <MenuItem value={OrderStatus.Completed}>Completed</MenuItem>
+                        <MenuItem value={OrderStatus.Delivered}>Delivered</MenuItem>
+                        <MenuItem value={OrderStatus.Cancelled}>Cancelled</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Box>
+                  <Button
+                    size="large"
+                    variant="contained"
+                    color="primary"
+                    disabled={status >= OrderStatus.Delivered}
+                    onClick={() =>
+                      mutate({
+                        id: orderDetails?.id || '',
+                        status: status,
+                      })
+                    }
+                  >
+                    Update
+                  </Button>
+                </>
+              )}
+            </>
+          ) : (
+            <Chip label={OrderStatus[orderDetails?.status || 0]} color="primary" />
+          )}
+        </Box>
         <Box className={classes.invoiceWrapper}>
           <Grid container justifyContent="space-between" spacing={4} className={classes.margin}>
             <Grid item xs={12} sm={8} md={6}>
@@ -77,13 +152,18 @@ export const Invoice: PrivateRouteComponent = ({ viewer }) => {
                 #{orderId}
               </Typography>
               <Typography variant="subtitle2" color="textSecondary">
-                Order Placed At : {new Date().toDateString()}
+                Order Placed At :{' '}
+                {orderDetails?.createdAt ? new Date(orderDetails?.createdAt).toDateString() : '---'}
               </Typography>
               <Typography variant="subtitle2" color="textSecondary">
-                Order Delivered At : {new Date().toDateString()}
+                Order Delivered At :{' '}
+                {orderDetails?.deliveredAt
+                  ? new Date(orderDetails?.deliveredAt).toDateString()
+                  : '---'}
               </Typography>
               <Typography variant="subtitle2" color="textSecondary">
-                Order Status : Delivered
+                Order Status :{' '}
+                {orderDetails?.status !== undefined ? OrderStatus[orderDetails?.status] : '---'}
               </Typography>
             </Grid>
             <Grid item xs={12} md={6}>
